effb63646011a7251542226714f9382586c478b4
ahmad <72674221+WhereDidMyDivGo@users.noreply.github.com>
2025-06-08 14:57:42 +0400

    Refactor layout and styles for Main component; update structure and improve responsiveness

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -11,17 +11,19 @@ function Main() {
 
         <div className="field-1">
           <img src="src/assets/images/image-computer.png" />
-          <div className="div1">
-            <h1>Quick Search</h1>
-            <p>Easily search your snippets by content, category, web address, application, and more.</p>
-          </div>
-          <div className="div2">
-            <h1>iCloud Sync</h1>
-            <p>Instantly saves and syncs snippets across all your devices.</p>
-          </div>
-          <div className="div3">
-            <h1>Complete History</h1>
-            <p>Retrieve any snippets from the first moment you started using the app.</p>
+          <div className="text-field-2">
+            <div>
+              <h1>Quick Search</h1>
+              <p>Easily search your snippets by content, category, web address, application, and more.</p>
+            </div>
+            <div>
+              <h1>iCloud Sync</h1>
+              <p>Instantly saves and syncs snippets across all your devices.</p>
+            </div>
+            <div>
+              <h1>Complete History</h1>
+              <p>Retrieve any snippets from the first moment you started using the app.</p>
+            </div>
           </div>
         </div>
       </div>
